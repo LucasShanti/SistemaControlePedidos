ad413d6ffe47279bc8f6fbe025c0aebd3c70c5e5
isaacmmendes312 <isaacmmendes312@gmail.com>
2025-11-17 18:20:59 -0300

    feature(cliente): structs e validacao de CPF

--- include/cliente.h
+++ include/cliente.h
@@ -1,21 +1,45 @@
+#ifndef cliente_h //se a variável cliente_h já foi definida, vai direto pro endif
+#define cliente_h
+
+typedef struct {
+    char nome[100];
+    char cpf[15];
+    char celular[20];
+} PessoaFisica;
+typedef struct {
+    char razaoSocial[150];
+    char cnpj[19];
+    char nomeContato[100];
+} PessoaJuridica;
+
+typedef enum {
+    tipo_PF, // valor 0
+    tipo_PJ // valor 1
+} TipoCliente; //é só pra definir os tipos de cliente
+
 typedef struct {
     int id;
-    char nome  [100];
-    char endereco [100];
-    char telefone [20];
-    int tipo; // 0 - Pessoa Fisica, 1 - Pessoa Juridica
-    PessoaFisica pf;
-    PessoaJuridica pj;
+    char endereco[200];
+    char telefone[20];
+    char email[100];
+
+    TipoCliente tipo; // indica se é PF ou PJ
+
+    union {
+        PessoaFisica pf;
+        PessoaJuridica pj;
+    } dados;
+
 } Cliente;
 
-typedef struct {
-    char cpf[15];
-    char nome[50];
-    char dataNascimento[11];
-} PessoaFisica;
+int validarCPF(char* cpf); //função para validar CPF (cliente.c)
+int validarCNPJ(char* cnpj); //função para validar CNPJ (cliente.c)
 
-typedef struct {
-    char cnpj[20];
-    char razaoSocial[50];
-    char nome [20];
-} PessoaJuridica;
+void cadastrarCliente();
+void consultarCliente();
+void removerCliente();
+void listarClientes();
+void analisarCliente(); //CRUD
+
+
+#endif 
--- src/cliente.c
+++ src/cliente.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "cliente.h"
 
-typedef struct {
-    int id;
-    char nome  [100];
-    char endereco [100];
-    char telefone [20];
-} Cliente;
+int validarCPF(char* cpf) {
+    int i, j, digito1 = 0, digito2 = 0;
+    int numeros[11];
+    int pos = 0;
+
+    for (i = 0; i < strlen(cpf); i++) {
+        if (isdigit(cpf[i])) {
+            numeros[pos] = cpf[i] - '0';
+            pos++;
+        }
+    }
+
+    if (pos != 11) {
+        return 0; // é falso
+    }
+
+    for (i = 0; i < 10; i++) {
+        if (numeros[i] != numeros[i+1]) {
+            break; // se o número for diferente, o CPF pode ser válido
+        }
+    }
+    if (i == 10) {
+        return 0; // se os números forem todos iguais, CPF inválido
+    }
+
+    // calculo do primeiro dígito verificador
+    for (i = 0, j = 10; i < 9; i++, j--) {
+        digito1 += numeros[i] * j;
+    }
+    digito1 = (digito1 % 11);
+    if (digito1 < 2) {
+        digito1 = 0;
+    } else {
+        digito1 = 11 - digito1;
+    }
+
+    // calculo do segundo dígito verificador
+    for (i = 0, j = 11; i < 10; i++, j--) {
+        digito2 += numeros[i] * j;
+    }
+    digito2 = (digito2 % 11);
+    if (digito2 < 2) {
+        digito2 = 0;
+    } else {
+        digito2 = 11 - digito2;
+    }
+
+    if (numeros[9] == digito1 && numeros[10] == digito2) {
+        return 1; // Verdadeiro (CPF válido)
+    } else {
+        return 0; // Falso (CPF inválido)
+    }
+}
