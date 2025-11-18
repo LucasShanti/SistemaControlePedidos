e3b977d874b86032f8b45fcf9c7de9c31dbf35cf
isaacmmendes312 <isaacmmendes312@gmail.com>
2025-11-18 15:14:25 -0300

    feature(cliente): função de validação de CNPJ

--- src/cliente.c
+++ src/cliente.c
@@ -57,3 +57,69 @@ int validarCPF(char* cpf) {
         return 0; // Falso (CPF inválido)
     }
 }
+
+int validarCNPJ(char* cnpj) {
+    int i, j, digito1 = 0, digito2 = 0;
+    int numeros[14];
+    int pos = 0;
+
+    for (i = 0; i < strlen(cnpj); i++) {
+        if (isdigit(cnpj[i])) {
+            numeros[pos] = cnpj[i] - '0';
+            pos++;
+        }
+    }
+
+    if (pos != 14) {
+        return 0; // é falso
+    }
+
+    for (i = 0; i < 13; i++) {
+        if (numeros[i] != numeros[i+1]) {
+            break; // se o número for diferente, o CNPJ pode ser válido
+        }
+    }
+
+    if (i == 13) {
+        return 0; // se os números forem todos iguais, CNPJ inválido
+    }
+
+    int multiplicador = 5;
+    for (i = 0; i < 12; i++) {
+        digito1 += numeros[i] * multiplicador;
+        multiplicador--;
+        if (multiplicador < 2) {
+            multiplicador = 9;
+        }
+    }
+
+    digito1 = (digito1 % 11);
+    if (digito1 < 2) {
+        digito1 = 0;
+    } else {
+        digito1 = 11 - digito1;
+    }
+
+    multiplicador = 6;
+    for (i = 0; i < 13; i++) {
+        digito2 += numeros[i] * multiplicador;
+        multiplicador--;
+        if (multiplicador < 2) {
+            multiplicador = 9;
+        }
+    }
+
+    digito2 = (digito2 % 11);
+    if (digito2 < 2) {
+        digito2 = 0;
+    } else {
+        digito2 = 11 - digito2;
+    }
+
+    if (numeros[12] == digito1 && numeros[13] == digito2) {
+        return 1; // Verdadeiro (CNPJ válido)
+    } else {
+        return 0; // Falso (CNPJ inválido)
+    }
+}
+
